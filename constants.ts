5c60eeb55d111ea3c6fda60e20cfcf0401df9bd6
Huangweize <126.com>
2026-02-03 23:27:07 +0800

    优化图片：使用WebP格式替代PNG/JPG，减小文件大小

--- constants.ts
+++ constants.ts
@@ -1,19 +1,19 @@
 export const PRESET_PERSON_IMAGES = [
-  "/images/person/person1.png",
-  "/images/person/person2.png",
-  "/images/person/person3.jpeg",
-  "/images/person/person4.png",
-  "/images/person/person5.png",
-  "/images/person/person6.jpg"
+  "/images/person/person1.webp",
+  "/images/person/person2.webp",
+  "/images/person/person3.webp",
+  "/images/person/person4.webp",
+  "/images/person/person5.webp",
+  "/images/person/person6.webp"
 ];
 
 export const PRESET_CLOTHES_IMAGES = [
-  "/images/clothes/clothes1.png",
-  "/images/clothes/clothes2.png",
-  "/images/clothes/clothes3.png",
-  "/images/clothes/clothes4.png",
-  "/images/clothes/clothes5.png",
-  "/images/clothes/clothes6.png"
+  "/images/clothes/clothes1.webp",
+  "/images/clothes/clothes2.webp",
+  "/images/clothes/clothes3.webp",
+  "/images/clothes/clothes4.webp",
+  "/images/clothes/clothes5.webp",
+  "/images/clothes/clothes6.webp"
 ];
 
 export const MODEL_NAME = 'gemini-2.5-flash-image';
